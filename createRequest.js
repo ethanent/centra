79ccb631691cfe91ac78652acf434ba0b86b73e7
Nicolas JESSEL <akanass@icloud.com>
2019-05-30 15:23:49 +0200

    Fix import

--- createRequest.js
+++ createRequest.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-const CentraRequest = require('./model/CentraRequest.js'))
+const CentraRequest = require('./model/CentraRequest.js')
 
 module.exports = (url, method) => {
 	return new CentraRequest(url, method)
